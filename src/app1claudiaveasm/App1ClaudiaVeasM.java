7cff2dc4cbe86f5efa427a3276ec215855fb7a2b
claudialm <claudia_incondicional@hotmail.com>
2017-10-25 21:09:41 -0300

    APP1 nuevo creado

---
+++ src/app1claudiaveasm/App1ClaudiaVeasM.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app1claudiaveasm;
+
+/**
+ *
+ * @author LAB03
+ */
+public class App1ClaudiaVeasM 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        int numero1;
+        float numero2;
+        
+        numero1=250;
+        numero2=1.25f;
+        
+        System.out.println("Suma de numero1 y numero2: "+(numero1+numero2));
+        System.out.println("Resta de numero1 y numero2: "+(numero1-numero2));
+        System.out.println("Multiplicacion de numero1 y numero2: "+(numero1*numero2));
+        System.out.println("Division de numero1 y numero2: "+(numero1/numero2));
+        System.out.println("Resto de numero1 y numero2: "+(numero1%numero2));
+    // TODO code application logic here
+    }
+    
+}
